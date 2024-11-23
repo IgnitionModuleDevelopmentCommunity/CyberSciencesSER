c82ad291dcd3f8bc0e46c051fc98e0ac2fad04ff
Travis Cox <travis@inductiveautomation.com>
2024-11-23 10:29:45 -0800

    Delete SER-build/target directory

--- SER-build/target/sign-module.bat
+++
@@ -1,1 +0,0 @@
-java -jar module-signer.jar -keystore=imdc.pfx -keystore-pwd=imdc -alias=imdc -alias-pwd=imdc -chain=imdc.p7b -module-in=CyberSciences-SER-Module-unsigned.modl -module-out=CyberSciences-SER-Module-signed.modl
